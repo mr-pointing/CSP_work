6857b5b92f466ee64dff14c2836c108106a97a99
mr-pointing <rpointing@blmhs.org>
2026-06-02 18:25:18 -0400

    - Nearly finished wordle

--- test2.py
+++ test2.py
@@ -11,14 +11,18 @@ import turtle as t
 # Turtle Env Variables
 sc = t.Screen()
 sc.setup(900, 1000)
+sc.bgcolor("#121212")
 row_start_x = -275
 row_start_y = 300
+letter_start_x = -240
+letter_start_y = 230
 boxer = t.Turtle()
-boxer.color("black")
+boxer.color("#fcfffa")
 boxer.speed(0)
 boxer.hideturtle()
 
 
+# Game Functions
 def make_boxes(first_x, first_y):
     boxer.penup()
     boxer.goto(first_x, first_y)
@@ -34,48 +38,64 @@ def make_boxes(first_x, first_y):
         boxer.pendown()
 
 
-# Game Functions
+def word_checker(user_answer, target):
+    return_list = []
+    for i in range(len(target)):
+        if user_answer[i] == target[i]:
+            return_list.append(True)
+        elif user_answer[i] in target:
+            return_list.append("maybe")
+        else:
+            return_list.append(False)
+    print(return_list)
+    return return_list
+
+def get_color(ans):
+    if ans is True:
+        return "green"
+    elif ans is False:
+        return "#fcfffa"
+    else:
+        return "orange"
+
+def write_word(user_word, target, letter_x, letter_y):
+    answer = word_checker(user_word, target)
+    checks = []
+
+    for i in range(5):
+        print(checks)
+        boxer.penup()
+        boxer.goto(letter_x, letter_y)
+        boxer.pendown()
+        boxer.color(get_color(answer[i]))
+        boxer.write(user_choice[i], font=("Arial", 30, "bold"))
+        letter_x += 110
+        if answer[i] is True:
+            checks.append("y")
+        if len(checks) == 5:
+            break
+    return checks
 
 
 # Game Variables
 words = ["bytes", "mouse", "input", "logic", "array"]
-target = r.choice(words)
-
-
-# user_choice = t.textinput("Picka Word", "Insert word to be guessed")
-
-def word_checker(user_answer):
-    while user_answer != target:
-        for i in range(len(target)):
-            if user_answer[i] == target[i]:
-                print(f"{user_answer[i]} is correct and in the right spot")
-            elif user_answer[i] in target:
-                print(f"{user_answer[i]} is inside target and but in the wrong spot")
-            else:
-                print(f"{user_answer[i]} is not correct and not in the word")
-        user_answer = input("Guess your word: ")
-
-    print(f"Congrats on guessing {target}!")
-
+word_choice = r.choice(words)
 
 for i in range(5):
     make_boxes(row_start_x, row_start_y)
     row_start_y -= 100
 
-boxer.penup()
-boxer.goto(-240,230)
-boxer.pendown()
-boxer.write("A", font=("Arial", 30, "bold"))
-
-boxer.penup()
-boxer.goto(-130,230)
-boxer.pendown()
-boxer.write("B", font=("Arial", 30, "bold"))
-
-boxer.penup()
-boxer.goto(-130,230)
-boxer.pendown()
-boxer.write("B", font=("Arial", 30, "bold"))
+for i in range(5):
+    user_choice = t.textinput("Picka Word", "Insert word to be guessed").lower()
+    ans_list = write_word(user_choice, word_choice, letter_start_x, letter_start_y)
+    if len(ans_list) == 5:
+        user_restart = t.textinput("Congratulations!",
+                                   "Would you like to try again?\n(Yes for again, No for Quit.)").lower()
+        if user_restart == "no":
+            exit()
+        else:
+            pass
+    letter_start_y -= 100
 
 
 sc.mainloop()
